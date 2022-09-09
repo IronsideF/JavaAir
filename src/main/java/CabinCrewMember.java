c2a575e65474ead1552f3ad0515d40f42a83ce87
Nathaniel Forsyth <ironsideforsyth@gmail.com>
2022-09-09 11:17:57 +0100

    Passenger and CCM classes built, initial tests passing

--- src/main/java/CabinCrewMember.java
+++ src/main/java/CabinCrewMember.java
@@ -1,3 +1,17 @@
 public class CabinCrewMember {
+    private String name;
+    private Rank rank;
 
+    public CabinCrewMember(String name, Rank rank) {
+        this.name = name;
+        this.rank = rank;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Rank getRank() {
+        return rank;
+    }
 }
